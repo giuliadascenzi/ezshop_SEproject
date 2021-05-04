7e9311be0333a1bff1978b88db503d6395e38b39
Antonino Monti s287037 <s287037@studenti.polito.it>
2021-05-04 17:55:06 +0200

    Implemented EZBalanceOperation, EZSaleTransaction and EZTicketEntry classes

--- src/main/java/it/polito/ezshop/data/classes/EZBalanceOperation.java
+++ src/main/java/it/polito/ezshop/data/classes/EZBalanceOperation.java
@@ -5,43 +5,48 @@ import it.polito.ezshop.data.BalanceOperation;
 import java.time.LocalDate;
 
 public class EZBalanceOperation implements BalanceOperation {
+    int balanceId;
+    LocalDate date;
+    double money;
+    String type;
+
     @Override
     public int getBalanceId() {
-        return 0;
+        return this.balanceId;
     }
 
     @Override
     public void setBalanceId(int balanceId) {
-
+        this.balanceId = balanceId;
     }
 
     @Override
     public LocalDate getDate() {
-        return null;
+        return this.date;
     }
 
     @Override
     public void setDate(LocalDate date) {
-
+        this.date = date;
     }
 
     @Override
     public double getMoney() {
-        return 0;
+        return this.money;
     }
 
     @Override
     public void setMoney(double money) {
-
+        this.money = money;
     }
 
     @Override
     public String getType() {
-        return null;
+        return this.type;
     }
 
     @Override
     public void setType(String type) {
-
+        this.type = type;
     }
 }
