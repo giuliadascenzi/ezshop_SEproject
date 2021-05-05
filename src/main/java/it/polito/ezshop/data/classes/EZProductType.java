7c6c9196de04a9da5e2c237833cbb81064c0a43d
fridanco <francesco.difranco97@gmail.com>
2021-05-05 19:25:21 +0200

    EzProductT and User classes implemented.

--- src/main/java/it/polito/ezshop/data/classes/EZProductType.java
+++ src/main/java/it/polito/ezshop/data/classes/EZProductType.java
@@ -3,73 +3,83 @@ package it.polito.ezshop.data.classes;
 import it.polito.ezshop.data.ProductType;
 
 public class EZProductType implements ProductType {
+    String barCode;
+    String description;
+    Double sellPrice;
+    Integer quantity;
+    Integer productid;
+    Double discountRate;
+    String notes;
+    String location;
     @Override
     public Integer getQuantity() {
-        return null;
+        return quantity;
     }
 
     @Override
     public void setQuantity(Integer quantity) {
-
+        this.quantity=quantity;
     }
 
     @Override
     public String getLocation() {
-        return null;
+        return location;
     }
 
     @Override
     public void setLocation(String location) {
-
+        this.location=location;
     }
 
     @Override
     public String getNote() {
-        return null;
+
+        return notes;
     }
 
     @Override
     public void setNote(String note) {
-
+        this.notes=note;
     }
 
     @Override
     public String getProductDescription() {
-        return null;
+
+        return description;
     }
 
     @Override
     public void setProductDescription(String productDescription) {
-
+        this.description=productDescription;
     }
 
     @Override
     public String getBarCode() {
-        return null;
+        return barCode;
     }
 
     @Override
     public void setBarCode(String barCode) {
-
+        this.barCode=barCode;
     }
 
     @Override
     public Double getPricePerUnit() {
-        return null;
+        return sellPrice;
     }
 
     @Override
     public void setPricePerUnit(Double pricePerUnit) {
-
+        this.sellPrice=pricePerUnit;
     }
 
     @Override
     public Integer getId() {
-        return null;
+        return productid;
     }
 
     @Override
     public void setId(Integer id) {
-
+        this.productid=id;
     }
 }
--- src/main/java/it/polito/ezshop/data/classes/EZUser.java
+++ src/main/java/it/polito/ezshop/data/classes/EZUser.java
@@ -2,44 +2,58 @@ package it.polito.ezshop.data.classes;
 
 import it.polito.ezshop.data.User;
 
+import java.util.Locale;
+
 public class EZUser implements User{
+    Integer userId;
+    String username;
+    String password;
+    UserRole role;
+
     @Override
     public Integer getId() {
-        return null;
+        return userId;
     }
 
     @Override
     public void setId(Integer id) {
-
+        this.userId=id;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return username;
     }
 
     @Override
     public void setUsername(String username) {
-
+            this.username=username;
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return password;
     }
 
     @Override
     public void setPassword(String password) {
+        this.password=password;
 
     }
 
     @Override
     public String getRole() {
-        return null;
+
+        return String.valueOf(role);
     }
 
     @Override
     public void setRole(String role) {
-
+        if((role).equalsIgnoreCase("MANAGER"))
+            this.role=UserRole.MANAGER;
+        if((role).equalsIgnoreCase("ADMINISTRATOR"))
+            this.role=UserRole.ADMINISTRATOR;
+        if((role).equalsIgnoreCase("CASHIER"))
+            this.role=UserRole.CASHIER;
     }
 }
