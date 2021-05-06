11d50e4d6e33c6a31ee06443708feee929725d36
giulia.dascenzi <giulia.dash@gmail.com>
2021-05-06 09:33:03 +0200

    EzCustomerCard implementati

---
+++ src/main/java/it/polito/ezshop/data/classes/EZCustomerCard.java
@@ -0,0 +1,36 @@
+package it.polito.ezshop.data.classes;
+
+public class EZCustomerCard {
+
+    String cardId;
+    Integer points;
+
+    public EZCustomerCard(String cardId) {
+        this.cardId = cardId;
+        this.points=0;
+    }
+
+    public EZCustomerCard() {
+        this.points=0;
+    }
+
+
+    public Integer getPoints() {
+        return points;
+    }
+
+    public void setPoints(Integer points) {
+        this.points = points;
+    }
+
+
+    public String getCardId() {
+        return cardId;
+    }
+
+    public void setCardId(String cardId) {
+        this.cardId = cardId;
+    }
+
+
+}
