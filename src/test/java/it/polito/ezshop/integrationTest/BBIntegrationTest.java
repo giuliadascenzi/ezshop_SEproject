c5df8e3e73cc5e9d71154aeccf4618657510241a
fridanco <francesco.difranco97@gmail.com>
2021-05-22 11:24:19 +0200

    TEST

--- src/test/java/it/polito/ezshop/integrationTest/BBIntegrationTest.java
+++ src/test/java/it/polito/ezshop/integrationTest/BBIntegrationTest.java
@@ -1,4 +1,9 @@
 package it.polito.ezshop.integrationTest;
 
+import it.polito.ezshop.data.EZShop;
+
+import static org.junit.Assert.assertTrue;
+
 public class BBIntegrationTest {
+    EZShop ez =new EZShop();
 }
