585a3cb92ea048248ad3fed7430ac189f435b2ae
fridanco <francesco.difranco97@gmail.com>
2021-05-22 18:40:13 +0200

    Test_CreateProductType

--- src/test/java/it/polito/ezshop/integrationTests/EZShopProductTypeTest.java
+++ src/test/java/it/polito/ezshop/integrationTests/EZShopProductTypeTest.java
@@ -16,27 +16,25 @@ public class EZShopProductTypeTest {
 
     }
 
-    public void UserLoginAdmin(){
-        try {
-            sp.createUser("fridanco","pass","Administrator");
-        } catch (InvalidUsernameException e) {
-            e.printStackTrace();
-        } catch (InvalidPasswordException e) {
-            e.printStackTrace();
-        } catch (InvalidRoleException e) {
-            e.printStackTrace();
-        }
-    }
-
 
 
     @Test
     public void Test_createProductType(){
-        //Test1
-        assertThrows(UnauthorizedException.class, ()->{sp.createProductType("Granarolo","6291041500213",1.5,"milk");});
-        UserLoginAdmin();
-
-
+        Integer id;
+        //TestAuthorization
+        try {
+            //No User Logged
+            assertThrows(UnauthorizedException.class, ()->{sp.createProductType("Granarolo","6291041500213",1.5,"milk");});
+            sp.createUser("fridanco1","pass","Cashier"); //Created,but no logged
+            assertThrows(UnauthorizedException.class, ()->{sp.createProductType("Granarolo","6291041500213",1.5,"milk");});
+            sp.login("fridanco1","pass");//Wrong Authorization
+            assertThrows(UnauthorizedException.class, ()->{sp.createProductType("Granarolo","6291041500213",1.5,"milk");});
+            sp.logout();
+            sp.createUser("fridanco","pass","Administrator");
+            sp.login("fridanco","pass");
+        } catch (InvalidUsernameException | InvalidPasswordException | InvalidRoleException e){
+            fail(e.getMessage());
+        }
         //Test-Description
         assertThrows(InvalidProductDescriptionException.class, ()->{sp.createProductType(null,"6291041500213",1.5,"milk");});
         assertThrows(InvalidProductDescriptionException.class, ()->{sp.createProductType("","6291041500213",1.5,"milk");});
@@ -46,9 +44,17 @@ public class EZShopProductTypeTest {
         assertThrows(InvalidProductCodeException.class, ()->{sp.createProductType("Granarolo","",1.5,"milk");});
         assertThrows(InvalidProductCodeException.class, ()->{sp.createProductType("Granarolo","I'm not a number",1.5,"milk");});
         assertThrows(InvalidProductCodeException.class, ()->{sp.createProductType("Granarolo","6291041500218",1.5,"milk");});
-        //Test-
+        //Test-PriceperUnit
+        assertThrows(InvalidPricePerUnitException.class,()->{sp.createProductType("Granarolo","6291041500213",-0.3,"milk");});
+        assertThrows(InvalidPricePerUnitException.class,()->{sp.createProductType("Granarolo","6291041500213",0,"milk");});
+        try {
+            id=sp.createProductType("Granarolo","6291041500213",1.5,"milk");
+            assertTrue(id>0);
+            id=sp.createProductType("Granarolo","6291041500213",1.5,"milk");
 
-
+        } catch (InvalidProductDescriptionException | InvalidProductCodeException | InvalidPricePerUnitException | UnauthorizedException e) {
+            fail(e.getMessage());
+        }
 
     }
 }
