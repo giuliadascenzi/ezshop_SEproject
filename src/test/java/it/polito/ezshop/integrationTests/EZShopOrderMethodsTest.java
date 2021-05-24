89ebbf72aaa529963cef1c7ede9980ee986dfbdf
giulia.dascenzi <giulia.dash@gmail.com>
2021-05-24 20:14:12 +0200

    IntegrationAPITestReport.md updated

--- src/test/java/it/polito/ezshop/integrationTests/EZShopOrderMethodsTest.java
+++ src/test/java/it/polito/ezshop/integrationTests/EZShopOrderMethodsTest.java
@@ -444,10 +444,10 @@ public class EZShopOrderMethodsTest {
             //add credit to have enough balance
             sp.recordBalanceUpdate(100);
             assertEquals(100, (int)sp.computeBalance());
-            assertEquals(2, (int) this.sp.issueOrder("6291041500213", 1, 50));
+            assertEquals(2, (int) this.sp.issueOrder("6291041500213", 2, 50));
             assertEquals(100, (int)sp.computeBalance());
             assertTrue(sp.payOrder(2));
-            assertEquals(50, (int)sp.computeBalance());
+            assertEquals(0, (int)sp.computeBalance());
 
 
 
